d2fca8b40fdd13debae5534ae1686af8be5bf9b0
Shreyas Nikam <158344330+shreyas-nikam@users.noreply.github.com>
2025-12-04 13:16:32 -0500

    Add app.py

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
-# Starter Python file
+import streamlit as st
+
+st.set_page_config(page_title="AI Security Vulnerability Simulation Lab", layout="wide")
+st.sidebar.image("https://www.quantuniversity.com/assets/img/logo5.jpg")
+st.sidebar.divider()
+st.title("AI Security Vulnerability Simulation Lab")
+st.divider()
+
+page = st.sidebar.selectbox(label="Navigation", options=["Introduction", "Setup", "Configuration", "Simulation", "Visualization"])
+
+if page == "Introduction":
+    from application_pages.introduction import main
+    main()
+elif page == "Setup":
+    from application_pages.setup import main
+    main()
+elif page == "Configuration":
+    from application_pages.configuration import main
+    main()
+elif page == "Simulation":
+    from application_pages.simulation import main
+    main()
+elif page == "Visualization":
+    from application_pages.visualization import main
+    main()
